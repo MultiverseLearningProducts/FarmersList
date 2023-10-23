698dd9039e0fb0f59d2ef14b7e1080dfd1e48273
ljenchik <pelagenkolena@gmail.com>
2023-10-23 12:44:31 +0100

    Product class created

---
+++ classes/Product.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(name, price, description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.inStock = true;
+    }
+    display() {
+        const dollar = "$";
+        return `Name: ${this.name}, Price: ${dollar} ${this.price}, Description: ${this.description}`;
+    }
+}
+
+module.exports = Product;
