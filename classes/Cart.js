5d0beee15f767789a34d54234b4d9c4105aecc31
Sameer <sameer.muhammad@sky.co.uk>
2023-10-23 15:07:24 +0100

    cart extension

--- classes/Cart.js
+++ classes/Cart.js
@@ -14,6 +14,17 @@ class Cart {
         this.products.splice(i, 1);
         this.total -= product.price;
     }
+    getTotal(){
+        let sum = 0
+        for(let i =0;i<this.products.length;i++){
+            sum += this.products[i].price
+        }
+        return sum
+    }
+    clear(){
+        this.products = []
+        this.total = 0
+    }
 }
 
 module.exports = Cart;
