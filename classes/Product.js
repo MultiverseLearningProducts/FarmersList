7b15275cfa536f8bae2dd056a68b34957e62a5b4
aaronmansfield5 <aaronmansfield12@gmail.com>
2023-10-23 13:01:53 +0100

    Lois' files

--- classes/Product.js
+++ classes/Product.js
@@ -1,12 +1,1 @@
-class Product {
-    constructor(n, p, d, i) {
-        this.n = n;
-        this.p = p;
-        this.d = d;
-        this.i = i;
-    }
-    display() {
-        return `Name: ${this.n}, Price: $${this.p}, Description: ${this.d}`;
-    }
-}
-module.exports = Product;
+class Product {constructor(n,p,d,i) {this.n=n;this.p=p;this.d=d;this.i=i;};display(){return `Name: ${this.n}, Price: $${this.p}, Description: ${this.d}`;}};module.exports=Product;
