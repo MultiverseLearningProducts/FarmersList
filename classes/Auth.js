b81b118bd83fa64ae8746959ff6ed16fed1878aa
Aaron Mansfield <37600872+aaronmansfield5@users.noreply.github.com>
2023-10-23 12:52:02 +0100

    Update Auth.js
    edit

--- classes/Auth.js
+++ classes/Auth.js
@@ -1,18 +1,1 @@
-const p = require("./Product.js");
-const ca = require("./Cart.js");
-const c = require("./Customer.js");
-
-class Auth {
-    constructor() {
-        this.cs = [];
-    }
-
-    register(n, e, sa) {
-        this.cs.push(c(n, e, sa));
-    }
-    login(e) {
-        return this.cs.find(c => e);
-    }
-}
-
-module.exports = Auth;
+const c = require("./Customer.js");class Auth {constructor(){this.cs = [];};register(n,e,sa) {this.cs.push(c(n,e,sa));};login(e){return this.cs.find(c=>e);}};module.exports = Auth;
