44bbd9dcad2cb533d180527e5ed4935312c61f7c
Mei Huang <meihuang8328@gmail.com>
2024-04-22 23:48:17 -0700

    Cart class completed

---
+++ classes/Cart.js
@@ -0,0 +1,21 @@
+class Cart {
+
+    constructor() {
+        this.products = []
+        this.total = 0
+    }
+
+    addProduct(product) {
+        this.products.push(product);
+        this.total += product.price;
+    }
+
+    removeProduct(i) {
+        let price = this.products[i].price;
+        this.total -= price
+
+        this.products.splice(i, 1)
+    }
+}
+
+module.exports = Cart
