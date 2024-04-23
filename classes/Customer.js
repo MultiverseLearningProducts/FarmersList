e4f7bac4f3c24805d2856f4259789c0c81bcfaaa
Mei Huang <meihuang8328@gmail.com>
2024-04-23 00:18:14 -0700

    completed rewardPoints and getRewardPoints logic

--- classes/Customer.js
+++ classes/Customer.js
@@ -5,10 +5,23 @@ class Customer {
         this.email = email
         this.shippingAddress = shippingAddress
         this.orderHistory = []
+        this.rewardPoints = 0
     }
 
     addToOrderHistory(cart) {
         this.orderHistory.push(cart)
+    }
+
+    getRewardPoints() {
+        for(let i = 0; i < this.orderHistory.length; i++) {
+            let cartItems = this.orderHistory[i].products
+            
+            for(let j = 0; j < cartItems.length; j++) {
+                let productRewardPoints = cartItems[j].rewardPoints
+
+                this.rewardPoints += productRewardPoints
+            }
+        }
     }
 }
 
