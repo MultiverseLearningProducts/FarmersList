54115ef0ef5111cdf37fbf29e0042e214eb3ab91
YawoSadji <yawo.sadji@gmx.com>
2024-09-09 17:53:40 -0400

    completed workshop

---
+++ classes/Auth.js
@@ -0,0 +1,21 @@
+const Customer = require('./Customer');
+
+class Auth{
+    constructor(){
+        this.customers = [];
+    }
+
+    register(name, email, shippingAddress){
+        const customer = new Customer(name, email, shippingAddress);
+        this.customers.push(customer);
+    }
+
+    login(email){
+        
+        const customer = this.customers.find(customer => customer.email === email);
+        
+      return customer || null;
+    }
+}
+
+module.exports = Auth;
